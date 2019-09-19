0e963bc2b75825e8c1d1cbf029d11bd7852688d1
Abi Vickery <vickeryabi@gmail.com>
2019-09-19 14:11:09 -0500

    test
    testing javascript function from a google doodle minigame made from the same function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-    // put function javascript stuff here!
+    
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-25773869-2']);
+  _gaq.push(['_setCookiePath', '/doodles/']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+   var ga = document.createElement('script');
+   ga.type = 'text/javascript'; ga.async = true;
+   ga.src = ('https:' == document.location.protocol ?
+             'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+   var s = document.getElementsByTagName('script')[0];
+   s.parentNode.insertBefore(ga, s);
+  })();
+
 });
